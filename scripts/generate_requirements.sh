4739fb3fd422943a2102699d2a28b899ee693b71
tillrostalski <rostis@hotmail.de>
2023-11-09 13:57:56 +0100

    actions

--- scripts/generate_requirements.sh
+++ scripts/generate_requirements.sh
@@ -1,1 +1,4 @@
-sed '/-e/d' requirements.lock > requirements.txt
+sed '/^-e/d' requirements.lock > requirements.tmp
+sed '/^-e/d' requirements-dev.lock >> requirements.tmp
+cat requirements.tmp > requirements.txt
+rm requirements.tmp
